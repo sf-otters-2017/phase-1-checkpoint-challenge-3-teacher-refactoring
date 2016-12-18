6999586f3a85df07f10b6b978ffa5de2020d24e7
Ethan Fertsch <efertsch@gmail.com>
2016-12-18 13:20:22 -0800

    Add payable module and include in Teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,9 @@
 require_relative 'person'
+require_relative 'payable'
 
 class Teacher < Person
+
+  include Payable
 
   attr_reader :minimum_rating, :response, :salary
 
@@ -13,15 +16,6 @@ class Teacher < Person
   def set_phase(num)
     @phase = num
     "Cool, I've always wanted to teach phase #{num}!"
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
